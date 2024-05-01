e3e128a723427c94efb6cd9b2c8ad1c34be147b3
UmerSufian <umersufian02@gmail.com>
2024-05-01 13:42:40 +0500

    change in styling

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -4,8 +4,8 @@ const Header = () => {
   return (
     <header className="bg-gray-800 text-white py-4">
       <div className="container mx-auto flex flex-col lg:flex-row items-center justify-between">
-        <div className="lg:w-2/3 lg:mr-4">
-          <h1 className="text-4xl lg:text-3xl font-bold leading-tight">
+        <div className="lg:w-2/3 lg: m-4">
+          <h1 className="text-2xl lg:text-3xl font-bold leading-tight">
             NewsPulse
           </h1>
           <p className="text-sm lg:text-base mt-2 lg:mt-0 lg:max-w-lg">
@@ -15,7 +15,7 @@ const Header = () => {
           </p>
         </div>
         <div>
-          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+          <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-4">
             Read More
           </button>
         </div>
